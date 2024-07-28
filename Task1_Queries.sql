ee6f121e914a1dc0eebb5eacd46255ff9412ae51
saharfayez <117713008+saharfayez@users.noreply.github.com>
2024-07-28 13:50:45 +0300

    Add files via upload

---
+++ Task1_Queries.sql
@@ -0,0 +1,37 @@
+-- Insert INTO User (UserName,UserAge,UserGraduationYear,UserGender,UserId,UserIdType)
+-- VALUES ('salma',27,2020,'female',2,'NID');
+
+-- Insert INTO Device (DeviceName,DeviceType,DeviceMacAddress,UserId,UserIdType)
+-- VALUES ('Laptop','Mac',5555,2,'NID');
+
+-- select * from Device;
+
+-- select * from User;
+
+-- select * from User where UserId=1;
+
+-- select count(UserId)
+-- from Device
+-- where UserId=1;
+
+-- DELETE from User where UserId=2;
+
+-- DELETE FROM Device WHERE DeviceMacAddress=4444;
+
+-- DELETE User FROM User JOIN Device
+-- WHERE User.UserId = Device.UserId
+-- AND Device.DeviceType='Mac';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
